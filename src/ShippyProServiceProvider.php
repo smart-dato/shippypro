21b6c482f8d34bcf3e01d3a6e0e5fe4c244058fd
Edgar Gomez <edgar.gomez@smart-dato.com>
2024-07-01 09:46:06 +0200

    Updated service provider

--- src/ShippyProServiceProvider.php
+++ src/ShippyProServiceProvider.php
@@ -4,22 +4,12 @@ namespace SmartDato\ShippyPro;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use SmartDato\ShippyPro\Commands\ShippyProCommand;
 
 class ShippyProServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
-        $package
-            ->name('shippypro')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_shippypro_table')
-            ->hasCommand(ShippyProCommand::class);
+        $package->name('shippypro')
+            ->hasConfigFile();
     }
 }
